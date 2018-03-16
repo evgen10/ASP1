a755c2bd37a46338d529cfd0c31577b0fc2e915b
evgen10 <i.am.mr.jackson96@gmail.com>
2018-03-16 16:58:27 +0600

    Changed show date

--- Task_1/Models/Initializer.cs
+++ Task_1/Models/Initializer.cs
@@ -10,7 +10,7 @@ namespace Task_1.Models
     {
         protected override void Seed(Context context)
         {
-            context.Students.Add(new Student { Name = "Valeriy", Birthdate = DateTime.Parse("18.05.1996") });
+            context.Students.Add(new Student { Name = "Valeriy", Birthdate = DateTime .Parse("18.05.1996") });
             context.Students.Add(new Student { Name = "Aleksey", Birthdate = DateTime.Parse("11.03.2000") });
             context.Students.Add(new Student { Name = "Sergey", Birthdate = DateTime.Parse("16.01.1993") });
             context.Students.Add(new Student { Name = "Irina", Birthdate = DateTime.Parse("20.11.1977") });
